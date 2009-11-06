0a60e8d3155bfc273f5ee4b7c1ee076b339ea210
justin.shepherd <galstrom21@gmail.com>
2009-11-06 17:41:15 -0600

    initial import

---
+++ memcache/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "Example Com"
+maintainer_email "ops@example.com"
+license          "Apache 2.0"
+description      "Installs/Configures memcache"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.1"
+
+attribute 'memcached/cache_size',
+  :default => "1024"
