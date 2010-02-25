8288d620d39241787ca9d88439a104a09e252d71
Justin Shepherd <galstrom21@gmail.com>
2010-02-24 21:27:48 -0600

    Corrected a mistake in the attributes file..
    Also included an example chef-solo json file.

--- memcached/attributes/memcache.rb
+++ memcached/attributes/memcache.rb
@@ -1,6 +1,6 @@
 set_unless[:memcached][:port] = 11211 
 set_unless[:memcached][:user] = "nobody" 
 set_unless[:memcached][:max_connections] = 1024 
-set[:memcached][:cache_size] = 64 
+set_unless[:memcached][:cache_size] = 64 
 set_unless[:memcached][:memcache_options] = "" 
 set_unless[:memcached][:address] = "127.0.0.1" 
