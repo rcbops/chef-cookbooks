d3d33ce4d7a5df5c151ea5bc829fc20f8096e127
Justin Shepherd <jshepherd@beastbox.local>
2010-03-01 11:23:32 -0600

    Error checking on max_clients and server_limit

--- httpd/attributes/httpd.rb
+++ httpd/attributes/httpd.rb
@@ -1,10 +1,10 @@
 
 set_unless[:httpd][:keepalive]			= "Off"
-set_unless[:httpd][:max_keepalive_requests]	= "100"
-set_unless[:httpd][:keepalive_timeout]		= "15"
-set_unless[:httpd][:start_servers]		= "8"
-set_unless[:httpd][:min_spare_servers]		= "5"
-set_unless[:httpd][:max_spare_servers]		= "20"
-set_unless[:httpd][:server_limit]		= "256"
-set_unless[:httpd][:max_clients]		= "256"
-set_unless[:httpd][:max_requests_per_child]	= "4000"
+set_unless[:httpd][:max_keepalive_requests]	= 100
+set_unless[:httpd][:keepalive_timeout]		= 15
+set_unless[:httpd][:start_servers]		= 8
+set_unless[:httpd][:min_spare_servers]		= 5
+set_unless[:httpd][:max_spare_servers]		= 20
+set_unless[:httpd][:server_limit]		= 256
+set_unless[:httpd][:max_clients]		= 256
+set_unless[:httpd][:max_requests_per_child]	= 4000
