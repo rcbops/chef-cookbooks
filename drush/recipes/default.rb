4bf0a8887bbdc54bd1ff08c0068bbe63324e6830
Justin Shepherd <jshepherd@beastbox.local>
2010-03-02 10:32:37 -0600

    Download, Extract, Install, and Delete drush

---
+++ drush/recipes/default.rb
@@ -0,0 +1,28 @@
+
+directory "/opt/drush" do
+	owner "root"
+	group "root"
+	mode "0755"
+	action :create
+	not_if "test -d /opt/drush"
+end
+
+remote_file "/tmp/drush-All-versions-3.0-beta1.tar.gz" do
+	source "http://ftp.drupal.org/files/projects/drush-All-versions-3.0-beta1.tar.gz"
+	mode "0644"
+	checksum "d4e62187"
+end
+
+execute "extract_drush" do
+	command "/usr/bin/tar -zxf /tmp/drush-All-versions-3.0-beta1.tar.gz -C /opt/drush"
+	action :nothing
+end
+
+execute "symlink_drush" do
+	command "/bin/ln -s /opt/drush/drush /usr/local/bin/drush"
+	action :nothing
+end
+
+file "/tmp/drush-All-versions-3.0-beta1.tar.gz" do
+	action :delete
+end
