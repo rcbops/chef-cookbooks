0a06c25749a9727cdde490399626cc3fa30dd230
Justin Shepherd <jshepherd@beastbox.local>
2010-03-03 13:48:56 -0600

    Working on breaking out php into clean sections.

--- httpd/recipes/php-drupal.rb
+++ httpd/recipes/php-drupal.rb
@@ -1,22 +1,6 @@
 
+include_recipe "httpd::php-minimal"
 include_recipe "mysql::client"
-include_recipe "memcached"
-
-package "php-common" do
-        action :install
-end
-
-package "php-cli" do
-        action :install
-end
-
-package "php" do
-        action :install
-end
-
-package "php-devel" do
-        action :install
-end
 
 package "php-gd" do
         action :install
@@ -34,36 +18,5 @@ package "php-pear" do
         action :install
 end
 
-package "php-pecl-apc" do
-        action :install
-end
-
-package "php-pecl-memcache" do
-        action :install
-end
-
-template "/etc/php.ini" do
-        source "php.ini.erb"
-        owner "root"
-        group "root"
-        mode "0644"
-        variables(
-	  :php_memory_limit => node[:httpd][:php_memory_limit]
-        )
-        notifies :restart, resources(:service => "httpd"), :delayed
-end
-
-template "/etc/php.d/apc.ini" do
-        source "apc.ini.erb"
-        owner "root"
-        group "root"
-        mode "0644"
-        variables(
-	  :apc_enabled => node[:httpd][:apc_enabled],
-	  :apc_shm_segments => node[:httpd][:apc_shm_segments],
-	  :apc_optimization => node[:httpd][:apc_optimization],
-	  :apc_enable_cli => node[:httpd][:apc_enable_cli],
-	  :apc_shm_size => node[:httpd][:apc_shm_size]
-        )
-        notifies :restart, resources(:service => "httpd"), :delayed
-end
+include_recipe "httpd::php-pecl-memcache"
+include_recipe "httpd::php-pecl-apc"
---
+++ httpd/recipes/php-minimal.rb
@@ -0,0 +1,27 @@
+
+package "php-common" do
+        action :install
+end
+
+package "php-cli" do
+        action :install
+end
+
+package "php" do
+        action :install
+end
+
+package "php-devel" do
+        action :install
+end
+
+template "/etc/php.ini" do
+        source "php.ini.erb"
+        owner "root"
+        group "root"
+        mode "0644"
+        variables(
+	  :php_memory_limit => node[:httpd][:php_memory_limit]
+        )
+        notifies :restart, resources(:service => "httpd"), :delayed
+end
