92b7156283f8880500437bad668f2e49ba7399d3
Justin Shepherd <jshepherd@beastbox.local>
2010-03-19 11:56:18 -0500

    fixing a typo

--- cookbooks/dns/attributes/default.rb
+++ cookbooks/dns/attributes/default.rb
@@ -20,7 +20,7 @@ dc_name_servers['lon1'].store("ns2","83.138.151.81")
 dc_name_servers['lon3'].store("ns1","83.138.151.80")
 dc_name_servers['lon3'].store("ns2","83.138.151.81")
 
-datacenter = File.read("/root/.rackspace/datacenter").gsub"\n",''()
+datacenter = File.read("/root/.rackspace/datacenter").gsub("\n",'')
 
 set_unless[:dns][:search_domain] = "somedomain.com"
 set_unless[:dns][:local_nameserver] = "127.0.0.1"
