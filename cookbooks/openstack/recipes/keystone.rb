ba4bec6dbc33808d44d3fb006fbb3573869cc8d2
Justin Shepherd <jshepher@rackspace.com>
2011-10-17 16:44:45 -0500

    forgot to set enabled and global flags on endpointTemplates

--- cookbooks/openstack/recipes/keystone.rb
+++ cookbooks/openstack/recipes/keystone.rb
@@ -74,7 +74,7 @@ execute "Keystone: add identity entpointTemplates" do
   node.set[:keystone][:adminURL] = "http://#{node[:ipaddress]}:5001/v2.0" 
   node.set[:keystone][:internalURL] = "http://#{node[:ipaddress]}:5000/v2.0"
   node.set[:keystone][:publicURL] = node[:keystone][:internalURL]
-  command "keystone-manage endpointTemplates add RegionOne identity #{node[:keystone][:publicURL]} #{node[:keystone][:adminURL]} #{node[:keystone][:internalURL]}"
+  command "keystone-manage endpointTemplates add RegionOne identity #{node[:keystone][:publicURL]} #{node[:keystone][:adminURL]} #{node[:keystone][:internalURL]} 1 1"
   action :run
   not_if "keystone-manage endpointTemplates list|grep 'identity'"
 end
@@ -84,7 +84,7 @@ execute "Keystone: add nova entpointTemplates" do
   node.set[:nova][:adminURL] = "http://#{node[:ipaddress]}:8774/v1.1/%tenant_id%" 
   node.set[:nova][:internalURL] = node[:nova][:adminURL]
   node.set[:nova][:publicURL] = node[:nova][:adminURL]
-  command "keystone-manage endpointTemplates add RegionOne nova #{node[:nova][:publicURL]} #{node[:nova][:adminURL]} #{node[:nova][:internalURL]}"
+  command "keystone-manage endpointTemplates add RegionOne nova #{node[:nova][:publicURL]} #{node[:nova][:adminURL]} #{node[:nova][:internalURL]} 1 1"
   action :run
   not_if "keystone-manage endpointTemplates list|grep 'nova'"
 end
@@ -94,7 +94,7 @@ execute "Keystone: add glance entpointTemplates" do
   node.set[:glance][:adminURL] = "http://#{node[:ipaddress]}:9292/v1.1/%tenant_id%" 
   node.set[:glance][:internalURL] = node[:glance][:adminURL] 
   node.set[:glance][:publicURL] = node[:glance][:adminURL]
-  command "keystone-manage endpointTemplates add RegionOne glance #{node[:glance][:publicURL]} #{node[:glance][:adminURL]} #{node[:glance][:internalURL]}"
+  command "keystone-manage endpointTemplates add RegionOne glance #{node[:glance][:publicURL]} #{node[:glance][:adminURL]} #{node[:glance][:internalURL]} 1 1"
   action :run
   not_if "keystone-manage endpointTemplates list|grep 'glance'"
 end
