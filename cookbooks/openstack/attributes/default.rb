21daec2aa8d11a75d95efbe0f9536d3c3f5b1391
Justin Shepherd <jshepher@rackspace.com>
2011-10-18 16:31:12 -0500

    creating dashboard database

--- cookbooks/openstack/attributes/default.rb
+++ cookbooks/openstack/attributes/default.rb
@@ -1,9 +1,15 @@
 default[:nova][:db] = "nova"
 default[:nova][:db_user] = "nova"
 default[:nova][:db_passwd] = "nova"
+
 default[:glance][:db] = "glance"
 default[:glance][:db_user] = "glance"
 default[:glance][:db_passwd] = "glance"
+
 default[:keystone][:db] = "keystone"
 default[:keystone][:db_user] = "keystone"
 default[:keystone][:db_passwd] = "keystone"
+
+default[:dash][:db] = "dash"
+default[:dash][:db_user] = "dash"
+default[:dash][:db_passwd] = "dash"
