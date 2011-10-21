d3f4cec629312caa60490a83ff56f0d6f984db9e
Justin Shepherd <jshepher@rackspace.com>
2011-10-21 15:26:14 -0500

    templating the kong config variables

---
+++ cookbooks/kong/attributes/default.rb
@@ -0,0 +1,21 @@
+default[:keystone][:host] = "127.0.0.1"
+default[:keystone][:service_port] = "5000"
+default[:keystone][:admin_port] = "5000"
+default[:keystone][:api_version] = "v2.0"
+default[:keystone][:user] = "admin"
+default[:keystone][:password] = "secrete"
+default[:keystone][:tenantid] = "openstack"
+default[:keystone][:region] = "RegionOne"
+
+default[:nova][:network_label] = "public"
+
+default[:rabbitmq][:host] = "127.0.0.1"
+default[:rabbitmq][:user] = "guest"
+
+default[:swift][:auth_host] = "127.0.0.1"
+default[:swift][:auth_port] = "443"
+default[:swift][:auth_prefix] = "/auth/"
+default[:swift][:auth_ssl] = "yes"
+default[:swift][:account] = "system"
+default[:swift][:username] = "root"
+default[:swift][:password] = "password"
