ad242d81638ebdf019eee638a4e32585eb2afc7b
Justin Shepherd <jshepher@rackspace.com>
2011-10-21 15:37:51 -0500

    installs in /opt

--- cookbooks/kong/recipes/default.rb
+++ cookbooks/kong/recipes/default.rb
@@ -13,41 +13,41 @@ end
 
 execute "git clone https://github.com/cloudbuilders/kong" do
   command "git clone https://github.com/cloudbuilders/kong"
-  cwd "/root"
+  cwd "/opt"
   user "root"
 end
 
 execute "generate swift_small object" do
   command "dd if=/dev/zero of=swift_small bs=512 count=1024"
-  cwd "/root/kong/include/swift_objects"
+  cwd "/opt/kong/include/swift_objects"
   user "root"
 end
 
 execute "generate swift_medium object" do
   command "dd if=/dev/zero of=swift_medium bs=512 count=1024000"
-  cwd "/root/kong/include/swift_objects"
+  cwd "/opt/kong/include/swift_objects"
   user "root"
 end
 
 execute "generate swift_large object" do
   command "dd if=/dev/zero of=swift_large bs=1024 count=1024"
-  cwd "/root/kong/include/swift_objects"
+  cwd "/opt/kong/include/swift_objects"
   user "root"
 end
 
 execute "grab the sample_vm" do
-  cwd "/root/kong/include/sample_vm"
+  cwd "/opt/kong/include/sample_vm"
   user "root"
   command "curl http://c250663.r63.cf1.rackcdn.com/ubuntu-11.04-server-uec-amd64.tar.gz | tar -zx"
 end
 
 execute "install virtualenv" do
   command "python tools/install_venv.py"
-  cwd "/root/kong"
+  cwd "/opt/kong"
   user "root"
 end
 
-template "/root/kong/etc/config.ini" do
+template "/opt/kong/etc/config.ini" do
   source "config.ini.erb"
   owner "root"
   group "root"
@@ -76,6 +76,6 @@ end
 
 execute "run kong" do
   command "./run_tests.sh -V"
-  cwd "/root/kong"
+  cwd "/opt/kong"
   user "root"
 end
