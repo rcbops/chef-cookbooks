e80bb213f56a81d1676eac2be1a415141168f3e6
Justin Shepherd <jshepher@rackspace.com>
2011-10-21 15:02:14 -0500

    recipe will now grab sample_vm

--- cookbooks/kong/recipes/default.rb
+++ cookbooks/kong/recipes/default.rb
@@ -16,16 +16,25 @@ end
 execute "generate swift_small object" do
   command "dd if=/dev/zero of=swift_small bs=512 count=1024"
   cwd "/root/kong/include/swift_objects"
+  user "root"
 end
 
 execute "generate swift_medium object" do
   command "dd if=/dev/zero of=swift_medium bs=512 count=1024000"
   cwd "/root/kong/include/swift_objects"
+  user "root"
 end
 
 execute "generate swift_large object" do
   command "dd if=/dev/zero of=swift_large bs=1024 count=1024"
   cwd "/root/kong/include/swift_objects"
+  user "root"
+end
+
+execute "grab the sample_vm" do
+  cwd "/root/kong/include/sample_vm"
+  user "root"
+  command "curl http://c250663.r63.cf1.rackcdn.com/ubuntu-11.04-server-uec-amd64.tar.gz | tar -zx"
 end
 
 execute "install virtualenv" do
