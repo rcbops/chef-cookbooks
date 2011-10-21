f9b954ea2d14724dfd699801a8953c6cdc6834e8
Justin Shepherd <jshepher@rackspace.com>
2011-10-21 14:56:39 -0500

    added creation of the swift objects

--- cookbooks/kong/recipes/default.rb
+++ cookbooks/kong/recipes/default.rb
@@ -13,6 +13,21 @@ execute "git clone https://github.com/cloudbuilders/kong" do
   user "root"
 end
 
+execute "generate swift_small object" do
+  command "dd if=/dev/zero of=swift_small bs=512 count=1024"
+  cwd "/root/kong/include/swift_objects"
+end
+
+execute "generate swift_medium object" do
+  command "dd if=/dev/zero of=swift_medium bs=512 count=1024000"
+  cwd "/root/kong/include/swift_objects"
+end
+
+execute "generate swift_large object" do
+  command "dd if=/dev/zero of=swift_large bs=1024 count=1024"
+  cwd "/root/kong/include/swift_objects"
+end
+
 execute "install virtualenv" do
   command "python tools/install_venv.py"
   cwd "/root/kong"
