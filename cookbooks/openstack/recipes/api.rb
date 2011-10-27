35a242684b26f31e2d2a158493a00ff2f166c652
Justin Shepherd <jshepher@rackspace.com>
2011-10-27 09:49:19 -0500

    nova-api requires python-keystone for keystone integration (api-paste.ini)

--- cookbooks/openstack/recipes/api.rb
+++ cookbooks/openstack/recipes/api.rb
@@ -19,6 +19,11 @@
 
 include_recipe "openstack::nova-common"
 
+package "python-keystone" do
+  action :install
+  options "--force-yes"
+end
+
 package "nova-api" do
   action :install
   options "--force-yes"
