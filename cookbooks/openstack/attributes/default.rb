01399c2b414ce24ebf721e8b49366a89ded1476e
Justin Shepherd <jshepher@rackspace.com>
2011-11-02 14:39:42 -0500

    fixing typo with package_release attribute

--- cookbooks/openstack/attributes/default.rb
+++ cookbooks/openstack/attributes/default.rb
@@ -36,7 +36,7 @@ default[:private][:bridge] = "br200"
 default[:private][:bridge_dev] = "eth3"
 
 default[:package_url] = "http://ops.monkeypuppetlabs.com/packages"
-default[:packate_release] = node[:lsb][:codename]
+default[:package_release] = node[:lsb][:codename]
 default[:package_component] = "diablo-d5"
 
 default[:controller_ipaddress] = node[:ipaddress]
