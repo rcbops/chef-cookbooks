7e40f06a774ba61e31179b0471dd54fb4b69a785
Jason Cannavale <jason@cannavale.com>
2011-12-14 20:45:47 -0800

    Fix README.md error in cookbooks/openstack/metadata.rb and remove recipe download from config/solo.rb. New tarball generated.

--- cookbooks/openstack/metadata.rb
+++ cookbooks/openstack/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Rackspace Hosting, Inc."
 license           "Apache 2.0"
 description       "Installs and configures Openstack"
-long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version           "1.0.0"
 # recipe            "mysql", "Includes the client recipe to configure a client"
 # recipe            "mysql::client", "Installs packages required for mysql clients using run_action magic"
