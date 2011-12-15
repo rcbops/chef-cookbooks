29a740625bf2a81f6df16834f2bbb36bc7a8823e
Jason Cannavale <jason@cannavale.com>
2011-12-14 20:45:47 -0800

    Fix README.md error in cookbooks/openstack/metadata.rb and remove recipe download from config/solo.rb. New tarball generated.

--- config/solo.rb
+++ config/solo.rb
@@ -8,6 +8,5 @@ file_cache_path    "/tmp/chef-solo/cookbooks"
 
 # Optionally store your JSON data file and a tarball of cookbooks remotely.
 #json_attribs "http://chef.example.com/dna.json"
-recipe_url    "http://github.com/galstrom21/Chef_Cookbooks/raw/master/cookbooks.tgz"
 
 Mixlib::Log::Formatter.show_time = false
