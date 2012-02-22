65299a5e49d360f4c0735e996c12c6e02e39c5c2
rpedde <ron@pedde.com>
2012-02-22 23:26:50 +0000

    glance image fixer-upper

--- cookbooks/openstack/attributes/default.rb
+++ cookbooks/openstack/attributes/default.rb
@@ -9,6 +9,7 @@ default[:glance][:db_user] = "glance"
 default[:glance][:db_passwd] = "glance"
 default[:glance][:api_port] = "9292"
 default[:glance][:registry_port] = "9191"
+default[:glance][:images] = [ "tty", "natty" ]
 
 default[:keystone][:db] = "keystone"
 default[:keystone][:db_user] = "keystone"
@@ -26,6 +27,7 @@ default[:dash][:db_passwd] = "dash"
 default[:image][:oneiric] = "http://c250663.r63.cf1.rackcdn.com/ubuntu-11.10-server-uec-amd64-multinic.tar.gz"
 default[:image][:natty] = "http://c250663.r63.cf1.rackcdn.com/ubuntu-11.04-server-uec-amd64-multinic.tar.gz"
 default[:image][:maverick] = "http://c250663.r63.cf1.rackcdn.com/ubuntu-10.10-server-uec-amd64-multinic.tar.gz"
+default[:image][:tty] = "http://smoser.brickies.net/ubuntu/ttylinux-uec/ttylinux-uec-amd64-12.1_2.6.35-22_1.tar.gz"
 
 default[:public][:label] = "public"
 default[:public][:ipv4_cidr] = "192.168.100.0/24"
