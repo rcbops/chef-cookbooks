da763a83ae5efa2993f0f681a28c2147a600b7f3
rpedde <ron@pedde.com>
2012-02-22 23:26:50 +0000

    glance image fixer-upper

--- cookbooks/openstack/recipes/glance.rb
+++ cookbooks/openstack/recipes/glance.rb
@@ -7,9 +7,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -100,16 +100,36 @@ template "/etc/glance/glance-scrubber.conf" do
   )
 end
 
-bash "default image setup" do
-  cwd "/tmp"
-  user "root"
-  code <<-EOH
-      mkdir images
-      curl #{node[:image][:natty]} | tar -zx -C images/
-      glance -A #{node[:keystone][:admin_token]} add name="ubuntu-11.04-kernel" disk_format=aki container_format=aki < images/natty-server-uec-amd64-vmlinuz-virtual
-      glance -A #{node[:keystone][:admin_token]} add name="ubuntu-11.04-initrd" disk_format=ari container_format=ari < images/natty-server-uec-amd64-loader
-      glance -A #{node[:keystone][:admin_token]} add name="ubuntu-11.04-server" disk_format=ami container_format=ami kernel_id=1 ramdisk_id=2 < images/natty-server-uec-amd64.img
+node[:glance][:images].each do |img|
+  bash "default image setup for #{img.to_s}" do
+    cwd "/tmp"
+    user "root"
+    code <<-EOH
+      set -e
+      set -x
+      mkdir -p images
 
+      curl #{node[:image][img.to_sym]} | tar -zx -C images/
+      image_name=$(basename #{node[:image][img]} .tar.gz)
+
+      image_name=${image_name%-multinic}
+
+      kernel_file=$(ls images/*vmlinuz-virtual | head -n1)
+      if [ ${#kernel_file} -eq 0 ]; then
+         kernel_file=$(ls images/*vmlinuz | head -n1)
+      fi
+
+      ramdisk=$(ls images/*-initrd | head -n1)
+      if [ ${#ramdisk} -eq 0 ]; then
+          ramdisk=$(ls images/*-loader | head -n1)
+      fi
+
+      kernel=$(ls images/*.img | head -n1)
+
+      kid=$(glance -A #{node[:keystone][:admin_token]} add name="${image_name}-kernel" disk_format=aki container_format=aki < ${kernel_file} | cut -d: -f2 | sed 's/ //')
+      rid=$(glance -A #{node[:keystone][:admin_token]} add name="${image_name}-initrd" disk_format=ari container_format=ari < ${ramdisk} | cut -d: -f2 | sed 's/ //')
+      glance -A #{node[:keystone][:admin_token]} add name="#{img.to_s}-image" disk_format=ami container_format=ami kernel_id=$kid ramdisk_id=$rid < ${kernel}
   EOH
-  not_if do File.exists?("/var/lib/glance/images/3") end
+    not_if "glance -A #{node[:keystone][:admin_token]} index | grep #{img.to_s}-image"
+  end
 end
