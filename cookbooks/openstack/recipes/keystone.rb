61d569e6295737561430be7bef0e3c3c69fa7345
Justin Shepherd <jshepher@rackspace.com>
2012-03-06 10:40:42 -0600

    templating default admin_token in keystone.conf, also these recipes require chef >= 0.10.8 due to the chef/shell_out require

--- cookbooks/openstack/recipes/keystone.rb
+++ cookbooks/openstack/recipes/keystone.rb
@@ -62,7 +62,8 @@ template "/etc/keystone/keystone.conf" do
             :ip_address => node[:controller_ipaddress],
             :db_name => node[:keystone][:db],
             :service_port => node[:keystone][:service_port],
-            :admin_port => node[:keystone][:admin_port]
+            :admin_port => node[:keystone][:admin_port],
+            :admin_token = node[:keystone][:admin_token]
             )
   notifies :restart, resources(:service => "keystone"), :immediately
   notifies :run, resources(:execute => "keystone-manage db_sync"), :immediately
