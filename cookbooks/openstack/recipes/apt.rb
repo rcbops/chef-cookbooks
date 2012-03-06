8e1c771dfe5fd7efeaa846c03a35acb2c5336fca
Justin Shepherd <jshepher@rackspace.com>
2012-03-06 13:40:46 -0600

    removing rcb specific package repo

--- cookbooks/openstack/recipes/apt.rb
+++ cookbooks/openstack/recipes/apt.rb
@@ -17,11 +17,16 @@
 # limitations under the License.
 #
 
-apt_repository "rcb-packages" do
-  uri "#{node[:package_url]}"
-  distribution node['lsb']['codename']
-  components [node[:package_component]]
-  keyserver "keyserver.ubuntu.com"
-  key "F87CBDE0"
-  action :add
+execute "apt-get update" do
+  command "apt-get update"
 end
+
+# This is no longer required since we are testing Cannonicals packages
+#apt_repository "rcb-packages" do
+#  uri "#{node[:package_url]}"
+#  distribution node['lsb']['codename']
+#  components [node[:package_component]]
+#  keyserver "keyserver.ubuntu.com"
+#  key "F87CBDE0"
+#  action :add
+#end
