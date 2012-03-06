a87425daae12d7ddbc40d4900242fbaeb631dc78
Justin Shepherd <jshepher@rackspace.com>
2012-03-06 10:48:12 -0600

    adding logging

--- cookbooks/openstack/recipes/keystone.rb
+++ cookbooks/openstack/recipes/keystone.rb
@@ -83,6 +83,7 @@ end
 execute "Keystone: add admin user" do
   cmd = Chef::ShellOut.new("#{keystone_cmd} tenant-list | grep openstack | cut -d' ' -f2")
   tenant = cmd.run_command
+  Chef::Log.info tenant.stdout
   command "#{keystone_cmd} user-create --name admin --pass secrete --tenant-id #{tenant.stdout} --enabled true"
   action :run
   not_if "#{keystone_cmd} user-list |grep admin"
