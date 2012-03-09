247cd12ff85728cf1e6b9e061c23ff0d564e2067
Justin Shepherd <jshepher@rackspace.com>
2012-03-08 20:23:44 -0600

    fixing typo

--- cookbooks/keystone/resources/register.rb
+++ cookbooks/keystone/resources/register.rb
@@ -10,7 +10,7 @@ attribute :auth_token, :kind_of => String
 
 # :create_service specific attributes
 attribute :service_type, :kind_of => String, :equal_to => [ "image", "identity", "compute", "storage", "ec2" ]
-attribute :sertice_name, :kind_of => String
+attribute :service_name, :kind_of => String
 attribute :service_description, :kind_of => String
 
 # :create_endpoint specific attributes
