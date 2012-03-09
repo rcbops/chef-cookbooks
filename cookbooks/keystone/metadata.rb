480ba18d169d5963fa8082549a4d16004d685f15
Justin Shepherd <jshepher@rackspace.com>
2012-03-08 21:19:14 -0600

    removing unnecessary shell_out requirement, and openstack::apt include. Extending sleep to 30s after keystone restart.

---
+++ cookbooks/keystone/metadata.rb
@@ -0,0 +1,12 @@
+maintainer        "Rackspace Hosting, Inc."
+license           "Apache 2.0"
+description       "Installs and configures the Keystone Identity Service"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "1.0.0"
+recipe            "keystone::server", "Installs packages required for a keystone server"
+
+%w{ ubuntu }.each do |os|
+  supports os
+end
+
+depends "openstack::mysql"
