34cf743f48063d8b43b36f76bca183e0f7113a2b
Jason Cannavale <jason.cannavale@rackspace.com>
2012-03-09 13:21:08 -0600

    Horizon cookbook addition

---
+++ cookbooks/horizon/recipes/server.rb
@@ -0,0 +1,63 @@
+#
+# Cookbook Name:: horizon
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe "apache2"
+include_recipe "apache2::mod_wsgi"
+include_recipe "openstack::apt"
+include_recipe "openstack::mysql"
+include_recipe "openstack::api"
+
+package "openstack-dashboard" do
+    action :upgrade
+end
+
+template "/etc/openstack-dashboard/local_settings.py" do
+  source "local_settings.py.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+            :user => node[:dash][:db_user],
+            :passwd => node[:dash][:db_passwd],
+            :ip_address => node[:controller_ipaddress],
+            :db_name => node[:dash][:db],
+            :service_port => node[:keystone][:service_port],
+            :admin_port => node[:keystone][:admin_port],
+            :admin_token => node[:keystone][:admin_token]
+            )
+end
+
+execute "openstack-dashboard syncdb" do
+  cwd "/usr/share/openstack-dashboard"
+  environment ({'PYTHONPATH' => '/etc/openstack-dashboard:/usr/share/openstack-dashboard:$PYTHONPATH'})
+  command "python manage.py syncdb"
+  action :run
+  not_if "/usr/bin/mysql -u root -e 'describe #{node["dash"][:db]}.django_content_type'"
+end
+
+template dashboard_for_platform(
+  [ "redhat", "centos" ] => { "default" => "#{node[:apache][:dir]}/vhost.d/openstack-dashboard" }, 
+  [ "ubuntu","debian" ] => { "default" => "#{node[:apache][:dir]}/sites-available/openstack-dashboard" },
+  "default" => { "default" => "#{node[:apache][:dir]}/openstack-dashboard" }
+  ) do
+  source "dash-site.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  variables(
+      :apache_contact => node[:apache][:contact],
+      :ssl_cert_file => node[:apache][:self_cert],
+      :ssl_key_file => node[:apache][:ssl_key_key],
+      :apache_log_dir => node[:apache][:log_dir]
+  )
+  if platform?("debian", "ubuntu") do
+    apache_site "openstack-dashboard" enable
+  end 
+
+  notifies :restart, resources(:service => "apache2")
+end
