191f5fe6252af4745858531d58f569291c2ecd7e
Justin Shepherd <jshepher@rackspace.com>
2012-03-08 20:02:13 -0600

    fixing typo

--- cookbooks/keystone/resources/register.rb
+++ cookbooks/keystone/resources/register.rb
@@ -1,16 +1,16 @@
 
 actions :create_service, :create_endpoint
 
-attribute :auth_protocol, :kind_of => string, :equal_to => [ "http", "https" ]
-attribute :auth_host, :kind_of => string
-attribute :auth_port, :kind_of => string
-attribute :api_ver, :kind_of => string, :default => "/v2.0"
-attribute :auth_token, :kind_of => string
+attribute :auth_protocol, :kind_of => String, :equal_to => [ "http", "https" ]
+attribute :auth_host, :kind_of => String
+attribute :auth_port, :kind_of => String
+attribute :api_ver, :kind_of => String, :default => "/v2.0"
+attribute :auth_token, :kind_of => String
 
 
 # :create_service specific attributes
-attribute :service_type, :kind_of => string, :equal_to => [ "image", "identity", "compute", "storage", "ec2" ]
-attribute :sertice_name, :kind_of => string
-attribute :service_description, :kind_of => string
+attribute :service_type, :kind_of => String, :equal_to => [ "image", "identity", "compute", "storage", "ec2" ]
+attribute :sertice_name, :kind_of => String
+attribute :service_description, :kind_of => String
 
 # :create_endpoint specific attributes
