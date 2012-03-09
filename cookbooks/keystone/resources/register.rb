b29fc084f7c3e41eaf528daa351426f2d6244e63
Justin Shepherd <jshepher@rackspace.com>
2012-03-08 21:42:04 -0600

    adding :create_endpoint specific attributes

--- cookbooks/keystone/resources/register.rb
+++ cookbooks/keystone/resources/register.rb
@@ -7,10 +7,14 @@ attribute :auth_port, :kind_of => String
 attribute :api_ver, :kind_of => String, :default => "/v2.0"
 attribute :auth_token, :kind_of => String
 
+attribute :service_type, :kind_of => String, :equal_to => [ "image", "identity", "compute", "storage", "ec2" ]
 
 # :create_service specific attributes
-attribute :service_type, :kind_of => String, :equal_to => [ "image", "identity", "compute", "storage", "ec2" ]
 attribute :service_name, :kind_of => String
 attribute :service_description, :kind_of => String
 
 # :create_endpoint specific attributes
+attribute :endpoint_region, :kind_of => String, :default => "RegionOne"
+attribute :endpoint_adminurl, :kind_of => String
+attribute :endpoint_internalurl, :kind_of => String
+attribute :endpoint_publicurl, :kind_of => String
