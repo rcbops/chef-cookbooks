951ec196d951ffbe18a8786071e270725230688a
Justin Shepherd <jshepher@rackspace.com>
2012-03-12 10:42:41 -0500

    Trying to use mysql lwrps to create_db, add user, set user pass

--- cookbooks/openstack/recipes/mysql.rb
+++ cookbooks/openstack/recipes/mysql.rb
@@ -16,76 +16,56 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+include mysql::client
+
+mysql_database "create nova database" do
+  host node[:controller_ip]
+  username "root"
+  password node['mysql']['server_root_password']
+  database node[:nova][:db]
+  action [:create_db]
+end
+
+mysql_database "create nova db user" do
+  host node[:controller_ip]
+  username "root"
+  password node['mysql']['server_root_password']
+  sql "grant all privileges on #{node[:nova][:db]}.* to '#{node[:nova][:db_user]}'@'%'"
+  action :query
+end
+
+mysql_database "configure nova db password" do
+  host node[:controller_ip]
+  username "root"
+  password node['mysql']['server_root_password']
+  sql "SET PASSWORD for '#{node[:nova][:db_user]}'@'%' = PASSWORD('#{node[:nova][:db_passwd]}')"
+  action :query
+end
+
 # extract the mysql root pass fropm the attributes file for easy reference 
 # later in the recipe
-root_pass = node['mysql']['root_pass']
-
-# set some preseed selections before installing mysql-server
-directory "/var/cache/local/preseeding" do
-  owner "root"
-  group node['mysql']['root_group']
-  mode 0755
-  recursive true
-end
-
-execute "preseed mysql-server" do
-  command "debconf-set-selections /var/cache/local/preseeding/mysql-server.seed"
-  action :nothing
-end
-
-template "/var/cache/local/preseeding/mysql-server.seed" do
-  cookbook "openstack"
-  source "mysql-server.seed.erb"
-  owner "root"
-  group node['mysql']['root_group']
-  mode "0600"
-  notifies :run, resources(:execute => "preseed mysql-server"), :immediately
-end
+#root_pass = node['mysql']['root_pass']
 
 
+#["nova", "glance", "keystone", "dash"].each do |service|
+#  execute "create #{service} database schema" do
+#    command "mysql -u root  -p#{root_pass} -e 'create database #{node[service][:db]}'"
+#    action :run
+#    not_if "/usr/bin/mysql -u root -p#{root_pass} -e 'show databases;'|grep '#{node[service][:db]}'"
+#  end
+#end
 
-# go ahead and install
-package "mysql-server" do
-  action :install
-end
+#["nova", "glance", "keystone", "dash"].each do |service|
+#  execute "create #{service} user" do
+#    command "mysql -u root -p#{root_pass} -e \"grant all privileges on #{node[service][:db]}.* to '#{node[service][:db_user]}'@'%'\""
+#    action :run
+#    not_if "mysql -u root -p#{root_pass} -e \"show grants for '#{node[service][:db_user]}'@'%'\""
+#  end
+#end
 
-service "mysql" do
-  supports :status => true, :restart => true
-  action [ :enable, :start ]
-end
-
-package "mysql-client" do
-  action :install
-end
-
-template "/etc/mysql/conf.d/nova-mysql.cnf" do
-  # conf.d settings will override my.cnf settings
-  source "nova-mysql.cnf.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :restart, resources(:service => "mysql"), :immediately
-end
-
-["nova", "glance", "keystone", "dash"].each do |service|
-  execute "create #{service} database schema" do
-    command "mysql -u root  -p#{root_pass} -e 'create database #{node[service][:db]}'"
-    action :run
-    not_if "/usr/bin/mysql -u root -p#{root_pass} -e 'show databases;'|grep '#{node[service][:db]}'"
-  end
-end
-
-["nova", "glance", "keystone", "dash"].each do |service|
-  execute "create #{service} user" do
-    command "mysql -u root -p#{root_pass} -e \"grant all privileges on #{node[service][:db]}.* to '#{node[service][:db_user]}'@'%'\""
-    action :run
-    not_if "mysql -u root -p#{root_pass} -e \"show grants for '#{node[service][:db_user]}'@'%'\""
-  end
-end
-
-["nova", "glance", "keystone", "dash"].each do |service|
-  execute "set #{service} user password" do
-    command "mysql -u root -p#{root_pass} -e \"SET PASSWORD for '#{node[service][:db_user]}'@'%' = PASSWORD('#{node[service][:db_passwd]}')\""
-    not_if "mysql -u root -e -p#{root_pass}\"select * from mysql.user where user='#{node[service][:db_user]}' and password=PASSWORD('#{node[service][:db_passwd]}')\\G\"| grep #{node[service][:db_user]}"
-  end
-end
+#["nova", "glance", "keystone", "dash"].each do |service|
+#  execute "set #{service} user password" do
+#    command "mysql -u root -p#{root_pass} -e \"SET PASSWORD for '#{node[service][:db_user]}'@'%' = PASSWORD('#{node[service][:db_passwd]}')\""
+#    not_if "mysql -u root -e -p#{root_pass}\"select * from mysql.user where user='#{node[service][:db_user]}' and password=PASSWORD('#{node[service][:db_passwd]}')\\G\"| grep #{node[service][:db_user]}"
+#  end
+#end
