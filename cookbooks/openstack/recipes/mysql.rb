14e6b6904e3d0a377b7f6d37b73c4b03000797e2
Justin Shepherd <jshepher@rackspace.com>
2012-03-12 11:06:58 -0500

    forgot to wrap mysql::client in quotes

--- cookbooks/openstack/recipes/mysql.rb
+++ cookbooks/openstack/recipes/mysql.rb
@@ -16,7 +16,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include_recipe mysql::client
+include_recipe "mysql::client"
 
 mysql_database "create nova database" do
   host node[:controller_ip]
