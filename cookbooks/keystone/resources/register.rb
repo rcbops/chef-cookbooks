2333290d0f939028f3528569670e7c4be54b76f8
Justin Shepherd <jshepher@rackspace.com>
2012-03-13 12:21:59 -0500

    adding new attributes for :create_user and :create_tenant actions

--- cookbooks/keystone/resources/register.rb
+++ cookbooks/keystone/resources/register.rb
@@ -1,5 +1,5 @@
 
-actions :create_service, :create_endpoint
+actions :create_service, :create_endpoint, :create_tenant, :create_user
 
 attribute :auth_protocol, :kind_of => String, :equal_to => [ "http", "https" ]
 attribute :auth_host, :kind_of => String
@@ -7,6 +7,7 @@ attribute :auth_port, :kind_of => String
 attribute :api_ver, :kind_of => String, :default => "/v2.0"
 attribute :auth_token, :kind_of => String
 
+# Used by both :create_service and :create_endpoint
 attribute :service_type, :kind_of => String, :equal_to => [ "image", "identity", "compute", "storage", "ec2" ]
 
 # :create_service specific attributes
@@ -18,3 +19,16 @@ attribute :endpoint_region, :kind_of => String, :default => "RegionOne"
 attribute :endpoint_adminurl, :kind_of => String
 attribute :endpoint_internalurl, :kind_of => String
 attribute :endpoint_publicurl, :kind_of => String
+
+# Used by both :create_tenant and :create_user
+attribute :tenant_name, :kind_of => String
+
+# :create_tenant specific attributes
+attribute :tenant_description, :kind_of => String
+attribute :tenant_enabled, :kind_of => String, :equal_to => [ "true", "false" ], :default => "true"
+
+# :create_user specific attributes
+attribute :user_name, :kind_of => String
+attribute :user_pass, :kind_of => String
+attribute :user_email, :kind_of => String
+attribute :user_enabled, :kind_of => String, :equal_to => [ "true", "false" ], :default => "true"
