dbc116588fe881630ebb7a8bc3a5cee1bcd5301d
Justin Shepherd <jshepher@rackspace.com>
2012-03-14 13:59:19 -0500

    was trying to get too fancy with the roles.. moved it up to an array

--- cookbooks/keystone/attributes/default.rb
+++ cookbooks/keystone/attributes/default.rb
@@ -7,11 +7,6 @@ default[:keystone][:service_port] = "5000"
 default[:keystone][:admin_port] = "35357"
 default[:keystone][:admin_token] = "999888777666"
 
-default[:keystone][:roles][:admin] = "admin"
-default[:keystone][:roles][:member] = "Member"
-default[:keystone][:roles][:keystoneadmin] = "KeystoneAdmin"
-default[:keystone][:roles][:keystoneserviceadmin] = "KeystoneServiceAdmin"
-default[:keystone][:roles][:sysadmin] = "sysadmin"
-default[:keystone][:roles][:netadmin] = "netadmin"
+default[:keystone][:roles] = [ "admin", "Member", "KeystoneAdmin", "KeystoneServiceAdmin", "sysadmin", "netadmin" ]
 
 default[:controller_ipaddress] = node[:ipaddress]
