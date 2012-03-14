50025bb54a2f8bd078e5a7a760b7d1297552ebdc
Justin Shepherd <jshepher@rackspace.com>
2012-03-14 13:42:03 -0500

    added [:keystone][:roles] as an array of roles to create in keystone

--- cookbooks/keystone/attributes/default.rb
+++ cookbooks/keystone/attributes/default.rb
@@ -7,4 +7,11 @@ default[:keystone][:service_port] = "5000"
 default[:keystone][:admin_port] = "35357"
 default[:keystone][:admin_token] = "999888777666"
 
+default[:keystone][:roles][:admin] = "admin"
+default[:keystone][:roles][:member] = "Member"
+default[:keystone][:roles][:keystoneadmin] = "KeystoneAdmin"
+default[:keystone][:roles][:keystoneserviceadmin] = "KeystoneServiceAdmin"
+default[:keystone][:roles][:sysadmin] = "sysadmin"
+default[:keystone][:roles][:netadmin] = "netadmin"
+
 default[:controller_ipaddress] = node[:ipaddress]
