91f5891f7987aab1619379ab7da7584dd05a4208
Justin Shepherd <jshepher@rackspace.com>
2012-03-14 21:42:36 -0500

    updating glance::registry to use :grant_role action

--- cookbooks/glance/attributes/default.rb
+++ cookbooks/glance/attributes/default.rb
@@ -8,6 +8,7 @@ default[:glance][:images] = [ "tty", "natty" ]
 default[:glance][:service_tenant_name] = "service"
 default[:glance][:service_user] = "glance"
 default[:glance][:service_pass] = "vARxre7K"
+default[:glance][:service_role] = "admin"
 
 default[:image][:oneiric] = "http://c250663.r63.cf1.rackcdn.com/ubuntu-11.10-server-uec-amd64-multinic.tar.gz"
 default[:image][:natty] = "http://c250663.r63.cf1.rackcdn.com/ubuntu-11.04-server-uec-amd64-multinic.tar.gz"
