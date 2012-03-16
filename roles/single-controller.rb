ac6ba16710701ec6ad11e4aacf49564de1000389
Justin Shepherd <jshepher@rackspace.com>
2012-03-16 10:08:33 -0500

    updating role[single-controller]

--- roles/single-controller.rb
+++ roles/single-controller.rb
@@ -1,6 +1,14 @@
 name "single-controller"
 description "Nova Controller (non-HA)"
 run_list (
-          "recipe[openstack::controller]"
+  "role[base]",
+  "recipe[mysql::server]",
+  "recipe[openstack::rabbitmq",
+  "recipe[keystone::server]",
+  "recipe[glance::default]",
+  "recipe[openstack::nova-setup]",
+  "recipe[openstack::scheduler]",
+  "recipe[openstack::api]",
+  "recipe[horizon::server]"
 )
 
