243f4dcd8b42facf0d0cf4980a0de635fca44e5d
Justin Shepherd <jshepher@rackspace.com>
2012-03-16 10:43:19 -0500

    adding external cookbook dependencies

--- cookbooks/horizon/metadata.rb
+++ cookbooks/horizon/metadata.rb
@@ -4,3 +4,8 @@ license          "All rights reserved"
 description      "Installs/Configures horizon"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+
+depends "openstack"
+depends "mysql"
+depends "database"
+depends "apache2"
