50b01f9075215409552c6230bf2f0eecec9df6a5
Justin Shepherd <jshepher@rackspace.com>
2012-03-16 10:10:25 -0500

    fixing typo

--- roles/single-controller.rb
+++ roles/single-controller.rb
@@ -3,7 +3,7 @@ description "Nova Controller (non-HA)"
 run_list (
   "role[base]",
   "recipe[mysql::server]",
-  "recipe[openstack::rabbitmq",
+  "recipe[openstack::rabbitmq]",
   "recipe[keystone::server]",
   "recipe[glance::default]",
   "recipe[openstack::nova-setup]",
