45fc9badde2269dc75b3760e4c88a6ddedf5881c
Justin Shepherd <jshepher@rackspace.com>
2012-03-16 10:10:39 -0500

    updating allinone to use role[single-compute]

--- roles/allinone.rb
+++ roles/allinone.rb
@@ -1,8 +1,6 @@
 name "allinone"
 description "This will create an all-in-one Openstack cluster"
 run_list(
-  "role[base]",
-  "recipe[openstack::apt]",
-  "recipe[openstack::controller]",
+  "role[single-controller]",
   "recipe[openstack::compute]"
 )
