841c24ba6df5870a937302bf950db879acdafe56
Kyle Bader <kyle.bader@dreamhost.com>
2012-03-22 07:39:09 -0700

    Cleanups for cookbook license and attribution.

--- cookbooks/nova/recipes/default.rb
+++ cookbooks/nova/recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: memcache
+# Cookbook Name:: nova
 # Recipe:: default
 #
-# Copyright 2009, Example Com
+# Copyright 2009, Rackspace Hosting, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
