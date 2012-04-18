b145e192362829515c99adb4e569130fd99b9c2e
galstrom21 <jshepher@rackspace.com>
2012-04-17 21:04:17 -0700

    dropping unused php5 cookbook

--- cookbooks/php5/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Rackspace Hosting"
-maintainer_email "hart.hoover@rackspace.com"
-license          "All rights reserved"
-description      "Installs/Configures PHP5"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
