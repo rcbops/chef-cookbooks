156fb31659ac74c4e6dd8b41cebd89c15ceff202
rpedde <ron@pedde.com>
2012-04-24 15:07:56 +0000

    update roles to refer to un-swifted names

--- roles/swift-object-server.rb
+++ roles/swift-object-server.rb
@@ -1,4 +1,4 @@
 name "swift-object-server"
 description "swift object server"
-run_list "recipe[swift::swift-object-server]"
+run_list "recipe[swift::object-server]"
 
--- roles/swift-proxy-server.rb
+++ roles/swift-proxy-server.rb
@@ -1,4 +1,4 @@
 name "swift-proxy-server"
 description "swift proxy server"
-run_list "recipe[swift::swift-proxy-server]"
+run_list "recipe[swift::proxy-server]"
 
