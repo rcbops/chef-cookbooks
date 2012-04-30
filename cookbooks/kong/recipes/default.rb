00ff7809b8f4b85889ba7bd1e43dde1c4505200c
Joseph W. Breu <breu@breu.org>
2012-04-30 16:41:12 +0100

    search enable kong

--- cookbooks/kong/recipes/default.rb
+++ cookbooks/kong/recipes/default.rb
@@ -58,22 +58,68 @@ execute "install virtualenv" do
   user "root"
 end
 
+if Chef::Config[:solo]
+  Chef::Log.warn("This recipe uses search. Chef Solo does not support search.")
+else
+  # Lookup keystone api ip address
+  keystone, start, arbitary_value = Chef::Search::Query.new.search(:node, "roles:keystone AND chef_environment:#{node.chef_environment}")
+  if keystone.length > 0
+    Chef::Log.info("kong/default: using search")
+      keystone_admin_username = "admin"
+      keystone_admin_password = keystone[0]['keystone']['users']['admin']['password']
+      keystone_admin_tenantname = keystone[0]['keystone']['users']['admin']['default_tenant']
+      keystone_internal_url = keystone[0]['keystone']['internalURL']
+      keystone_admin_url = keystone[0]['keystone']['adminURL']
+      keystone_admin_token = keystone[0]['keystone']['admin_token']
+      keystone_admin_port = keystone[0]['keystone']['admin_port']
+      keystone_region = keystone[0]['keystone']['admin_port']
+      ec2_access = keystone[0]["credentials"]["EC2"]["admin"]["access"]
+      ec2_secret = keystone[0]["credentials"]["EC2"]["admin"]["secret"]
+      keystone_api_ip = keystone[0]['keystone']['api_ipaddress']
+      keystone_service_port = keystone[0]['keystone']['service_port']
+  else
+    Chef::Log.info("kong/keystone: NOT using search")
+      keystone_admin_username = "admin"
+      keystone_admin_password = node['keystone']['users']['admin']['password']
+      keystone_admin_tenantname = node['keystone']['users']['admin']['default_tenant']
+      keystone_internal_url = node['keystone']['internalURL']
+      keystone_admin_url = node['keystone']['adminURL']
+      keystone_admin_token = node['keystone']['admin_token']
+      keystone_admin_port = node['keystone']['admin_port']
+      ec2_access = node["credentials"]["EC2"]["admin"]["access"]
+      ec2_secret = node["credentials"]["EC2"]["admin"]["secret"]
+      keystone_api_ip = node['keystone']['api_ipaddress']
+      keystone_service_port = node['keystone']['service_port']
+  end
+
+rabbit, something, arbitary_value = Chef::Search::Query.new.search(:node, "roles:rabbitmq-server AND chef_environment:#{node.chef_environment}")
+  if rabbit.length > 0
+    Chef::Log.info("kong/rabbitmq: using search")
+    rabbit_ip_address = rabbit[0]['ipaddress']
+    rabbit_user = rabbit[0]['rabbitmq']['default_user']
+  else
+    Chef::Log.info("nova-common/rabbitmq: NOT using search")
+    rabbit_ip_address = node['ipaddress']
+    rabbit_user = node['rabbitmq']['default_user']
+  end
+end
+
 template "/opt/kong/etc/config.ini" do
   source "config.ini.erb"
   owner "root"
   group "root"
   mode "0644"
   variables(
-    :ip_address => node[:controller_ipaddress],
-    :keystone_service_port => node[:keystone][:service_port],
-    :keystone_admin_port => node[:keystone][:admin_port],
+    :ip_address => keystone_api_ip,
+    :keystone_service_port => keystone_service_port,
+    :keystone_admin_port => keystone_admin_port,
     :keystone_apiver => node[:keystone][:api_version],
-    :keystone_user => node[:keystone][:user],
-    :keystone_pass => node[:keystone][:password],
-    :keystone_tenantid => node[:keystone][:tenantid],
-    :keystone_region => node[:keystone][:region],
+    :keystone_user => keystone_admin_username,
+    :keystone_pass => keystone_admin_password,
+    :keystone_tenantid => keystone_admin_tenantname,
+    :keystone_region => 'RegionOne',
     :nova_network_label => node[:nova][:network_label],
-    :rabbit_user => node[:rabbitmq][:user],
+    :rabbit_user => rabbit_user,
     :swift_auth_port => node[:swift][:auth_port],
     :swift_auth_prefix => node[:swift][:auth_prefix],
     :swift_ssl => node[:swift][:auth_ssl],
