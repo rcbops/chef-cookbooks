8f6f42c8e135b472bbc4bd80dd8d5a08e0acf800
Joseph W. Breu <breu@breu.org>
2012-07-02 09:28:50 -0500

    remove the monitoring roles for now

--- roles/allinone.rb
+++ roles/allinone.rb
@@ -3,7 +3,11 @@ description "This will create an all-in-one Openstack cluster"
 run_list(
   "role[single-controller]",
   "role[single-compute]",
-  "role[collectd-client]",
-  "role[collectd-server]",
-  "role[graphite]"
 )
+
+#
+# TODO(breu): These are commented out for now
+#
+# "role[collectd-client]",
+# "role[collectd-server]",
+# "role[graphite]"
