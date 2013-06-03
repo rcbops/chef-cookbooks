30c6ad60f784307cd509fcbef0cd08dde094ea5e
galstrom21 <jshepher@rackspace.com>
2013-06-02 19:50:28 -0500

    Also fixing single-controller-usage.rb

--- roles/single-controller-usage.rb
+++ roles/single-controller-usage.rb
@@ -1,19 +1,9 @@
-{
-  "name": "single-controller-usage",
-  "description": "",
-  "json_class": "Chef::Role",
-  "default_attributes": {
-  },
-  "override_attributes": {
-  },
-  "chef_type": "role",
-  "run_list": [
-    "role[single-controller]",
-    "role[ceilometer-setup]",
-    "role[ceilometer-api]",
-    "role[ceilometer-central-agent]",
-    "role[ceilometer-collector]"
-  ],
-  "env_run_lists": {
-  }
-}
+name "single-controller-usage"
+description ""
+run_list(
+  "role[single-controller]",
+  "role[ceilometer-setup]",
+  "role[ceilometer-api]",
+  "role[ceilometer-central-agent]",
+  "role[ceilometer-collector]"
+)
