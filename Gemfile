539e102cdb62b5af5f35944e700aceabd0a69ac6
paulczar <paul@paulcz.net>
2013-06-10 10:12:10 -0500

    Added support for testing with Vagrant+Berkshelf+Spiceweasel.
         * vagrantfile and provisioning scripts
         * berkshelf/spiceweasel
         * some work on chef-zero and more testing
         * Two VM vagrant configuration
    
    Created a Vagrantfile to create two scenarios  `chef` + `allinone` or `chef` + `single_compute` + `single_controller`.     Also uses `Berkshelf` to give an alternative to submodules and `Spiceweasel` to define the vagrant infrastructure.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem "chef", "~> 11.4.4"
+gem "berkshelf", "~> 1.4.3"
+gem "spiceweasel", "~> 2.2.0"
---
+++ extras/knife-zero.rb
@@ -0,0 +1,9 @@
+log_level                :info
+log_location             STDOUT
+node_name                'admin'
+client_key               '/vagrant/.chef/admin.pem'
+validation_client_name   'chef-validator'
+validation_key           '/vagrant/.chef/chef-validator.pem'
+chef_server_url          'http://33.33.33.50:8889'
+cache_type               'BasicFile'
+cache_options( :path => '/home/vagrant/.chef/checksums' )
