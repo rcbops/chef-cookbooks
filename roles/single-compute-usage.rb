b7b93e9ff69be999f12ea9534d212379e802744b
rackerjoe <breu@breu.org>
2013-09-09 13:01:58 -0500

    revert all of my changes in single-compute-usage.rb

--- roles/single-compute-usage.rb
+++ roles/single-compute-usage.rb
@@ -1,12 +1,6 @@
 name "single-compute-usage"
 description ""
 run_list(
-  "role[base]",
-  "recipe[osops-utils::keepalived-timeouts]",
-  "role[rsyslog-client]",
-  "role[nova-network-compute]",
   "role[single-compute]",
-  "role[ceilometer-compute]",
-  "recipe[openstack-monitoring::nova-compute]",
-  "role[openstack-logging]"
+  "role[ceilometer-compute]"
 )
