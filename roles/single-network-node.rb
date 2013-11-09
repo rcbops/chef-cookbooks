5264ebcacd1a54b46924f5cc15d4d62ab04c6ffa
William Kelly <the.william.kelly@gmail.com>
2013-11-09 16:13:35 -0600

    Temporarily disable rpcdaemon

--- roles/single-network-node.rb
+++ roles/single-network-node.rb
@@ -8,6 +8,6 @@ run_list(
   "recipe[nova-network::neutron-dhcp-agent]",
   "recipe[nova-network::neutron-plugin]",
   "recipe[nova-network::neutron-l3-agent]",
-  "recipe[nova-network::rpcdaemon]",
+#  "recipe[nova-network::rpcdaemon]",
   "role[openstack-logging]"
 )
