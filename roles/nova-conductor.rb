f6f8dd8e317c9754fc185fc5370d7c4746d59de0
William Kelly <the.william.kelly@gmail.com>
2013-11-11 14:10:11 -0600

    Patch amqplib on centos
    Conflicts:
    	roles/nova-conductor.rb

--- roles/nova-conductor.rb
+++ roles/nova-conductor.rb
@@ -3,6 +3,7 @@ description "Nova Conductor"
 run_list(
   "role[base]",
   "recipe[osops-utils::keepalived-timeouts]",
+  "recipe[osops-utils::centos-amqplib-keepalive-patch]",
   "recipe[nova::nova-conductor]",
   "recipe[openstack-monitoring::nova-conductor]"
 )
