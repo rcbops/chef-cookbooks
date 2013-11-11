7dadeb351482f5d4016db1d34e5b8a0e4fb6cc45
William Kelly <the.william.kelly@gmail.com>
2013-11-11 14:10:11 -0600

    Patch amqplib on centos

--- roles/single-compute.rb
+++ roles/single-compute.rb
@@ -3,6 +3,7 @@ description "Nova compute (with non-HA Controller)"
 run_list(
   "role[base]",
   "recipe[osops-utils::keepalived-timeouts]",
+  "recipe[osops-utils::centos-amqplib-keepalive-patch]",
   "role[rsyslog-client]",
   "role[nova-network-compute]",
   "recipe[nova::compute]",
