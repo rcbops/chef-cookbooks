6586e227cf804d6e56144f4e579e6b4d54f3239f
William Kelly <the.william.kelly@gmail.com>
2013-11-21 10:16:09 -0600

    Add l3-agent role

--- roles/single-network-node.rb
+++ roles/single-network-node.rb
@@ -7,6 +7,7 @@ run_list(
   "recipe[nova-network::quantum-metadata-agent]",
   "recipe[nova-network::quantum-dhcp-agent]",
   "recipe[nova-network::quantum-plugin]",
+  "recipe[nova-network::quantum-l3-agent",
   "recipe[nova-network::rpcdaemon]",
   "role[openstack-logging]"
 )
