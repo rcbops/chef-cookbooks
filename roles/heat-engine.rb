98feb37cd8c40f95d9ead908e245f639d83af933
rackerjoe <breu@breu.org>
2013-12-17 16:14:23 -0600

    role update to include procmon monitoring for heat

--- roles/heat-engine.rb
+++ roles/heat-engine.rb
@@ -2,5 +2,6 @@ name "heat-engine"
 description "heat engine"
 run_list(
   "role[base]",
-  "recipe[heat::heat-engine]"
+  "recipe[heat::heat-engine]",
+  "recipe[openstack-monitoring::heat-engine]"
 )
