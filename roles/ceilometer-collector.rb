e7f4db63b74b055109dc1f7e6f6d83ca5fc21cf7
Kevin Carter <kevin.carter@rackspace.com>
2013-12-17 16:44:52 -0600

    added monit recipies into the ceilometer roles

--- roles/ceilometer-collector.rb
+++ roles/ceilometer-collector.rb
@@ -2,6 +2,7 @@ name "ceilometer-collector"
 description "ceilometer collector server"
 run_list(
   "role[base]",
-  "recipe[ceilometer::ceilometer-collector]"
+  "recipe[ceilometer::ceilometer-collector]",
+  "recipe[openstack-monitoring::ceilometer-collector]"
 )
 
